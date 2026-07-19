d9ca89c1c2c92a4bcd589ed032ec09c7b60d3346
quz <46859244+quzq@users.noreply.github.com>
2026-07-19 09:58:32 +0900

    Add Firebase Google services plugin

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
     id("com.android.application") version "8.7.2" apply false
     id("org.jetbrains.kotlin.android") version "2.1.21" apply false
+    id("com.google.gms.google-services") version "4.5.0" apply false
 }
